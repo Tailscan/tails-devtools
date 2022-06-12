fa7abcd0d1e8715faa23507ff9e354d8fd8a4071
tungtbt <tungtbt@tungtbts-mini.dsldevice.lan>
2022-06-12 15:12:47 +0700

    feat: useCopy

--- src/pages/panel/hooks/use-copy.tsx
+++ src/pages/panel/hooks/use-copy.tsx
@@ -3,18 +3,36 @@ import { useState } from "react";
 type CopiedValue = string | null;
 type CopyFn = (text: string) => Promise<boolean>; // Return success
 
+function copyText(text: string) {
+  // creating textarea of html
+  const input = document.createElement("textarea");
+  //adding p tag text to textarea
+  input.value = text;
+  // Avoid scrolling to bottom
+  input.style.position = "fixed";
+  input.style.top = "-99999px";
+  input.style.left = "-99999px";
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand("Copy");
+  // removing textarea after copy
+  input.remove();
+
+  return true;
+}
+
 export function useCopyToClipboard(): [CopiedValue, CopyFn] {
-  const [copiedText, setCopiedText] = useState<CopiedValue>(null);
+  const [copiedText, setCopiedText] = useState<CopiedValue>("");
 
   const copy: CopyFn = async (text) => {
-    if (!navigator?.clipboard) {
+    if (!document.execCommand) {
       console.warn("Clipboard not supported");
       return false;
     }
 
     // Try to save to clipboard then save it in the state if worked
     try {
-      await navigator.clipboard.writeText(text);
+      copyText(text);
       setCopiedText(text);
       return true;
     } catch (error) {
